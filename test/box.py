9e89f8002460552a22906106902757163d8064c9
DoyunShin <60532811+DoyunShin@users.noreply.github.com>
2021-07-09 12:38:46 +0900

    update

--- test/box.py
+++ test/box.py
@@ -4,6 +4,7 @@ class dummy:
 class storage(Exception):
     def __init__(self):
         self.debug = False
+        self.test = True
         self.hand = hand(self)
         pass
 
@@ -25,11 +26,11 @@ class hand(Exception):
         self.rst = dummy()
         self.rst.x = dummy()
         self.rst.y = dummy()
-        self.rst.x.max = -40
+        self.rst.x.max = -20
         self.rst.x.min = -80
         self.rst.y.max = 0
         self.rst.y.min = -40
-        
+
 
     def capture(self):
         from threading import Thread
@@ -49,7 +50,7 @@ class hand(Exception):
         self.capture.set(self.opencv.CAP_PROP_FRAME_HEIGHT, 768)
         self.success, self.image = self.capture.read()
         while True:
-            try:
+            try:    
                 if not self.capture.isOpened(): break
                 self.success, self.image = self.capture.read()
             except AttributeError as e:
@@ -61,68 +62,123 @@ class hand(Exception):
             try: 
                 tmp = self.success
             except AttributeError as e:
-                print(e)
+                if self.storage.debug == True: print("DEBUG: "+str(e))
+                print("DEBUG: "+"Waiting for Camera output...")
+                while True:
+                    try:
+                        tmp = self.success
+                        break
+                    except AttributeError:
+                        if self.storage.debug == True: print("DEBUG: "+str(e))
+                        pass
                 continue
-            if not self.success:
-                print("Ignored empty camera frame")
+            if not self.success and self.storage.debug == True: print("DEBUG: Ignored empty camera frame")
             else:
                 image = self.opencv.cvtColor(self.opencv.flip(self.image, 1), self.opencv.COLOR_BGR2RGB)
-                image.flags.writeable = False
-                results = self.mediapipe.hands_conf.process(image)
-
-                image.flags.writeable = True
-                image = self.opencv.cvtColor(image, self.opencv.COLOR_RGB2BGR)
-                if results.multi_hand_landmarks:
-                    for hand_landmarks in results.multi_hand_landmarks:
-                        self.mediapipe.drawing.draw_landmarks(image, hand_landmarks, self.mediapipe.hands.HAND_CONNECTIONS)
-                
-                    rst = dummy()
-                    rst.x = dummy()
-                    rst.y = dummy()
-
-                    print(len(results.multi_hand_landmarks))
-                    for landmarks in results.multi_hand_landmarks:
-                        rst.x.max = 0
-                        rst.x.min = 1
-                        rst.y.max = 0
-                        rst.y.min = 1
-                        for landmark in landmarks.landmark:
-                            if landmark.x > rst.x.max: rst.x.max = landmark.x
-                            if landmark.x < rst.x.min: rst.x.min = landmark.x
-                            if landmark.y > rst.y.max: rst.y.max = landmark.y
-                            if landmark.y < rst.y.min: rst.y.min = landmark.y
-                        
-                        rst.x.max *= 1366
-                        rst.x.min *= 1366
-                        rst.y.max *= 768
-                        rst.y.min *= 768
-
-                        rst.x.max += self.rst.x.max
-                        rst.x.min += self.rst.x.min
-                        rst.y.max += self.rst.y.max
-                        rst.y.min += self.rst.y.min
-
-                        rst.x.max = int(rst.x.max)
-                        rst.x.min = int(rst.x.min)
-                        rst.y.max = int(rst.y.max)
-                        rst.y.min = int(rst.y.min)
-                        if self.storage.debug == True:
-                            print(rst.x.max)
-                            print(rst.x.min)
-                            print(rst.y.max)
-                            print(rst.y.min)
-                        
-                        if rst.x.max < self.rang[0] and rst.y.max < self.rang[1] and rst.x.min > self.rang[2] and rst.y.min > self.rang[3]:
-                            self.opencv.rectangle(image, (rst.x.max, rst.y.max), (rst.x.min, rst.y.min), (0,255,0), 3)
-                        else:
-                            self.opencv.rectangle(image, (rst.x.max, rst.y.max), (rst.x.min, rst.y.min), (0,0,0), 3)
-                        
-                #print(results.multi_hand_landmarks)
-                self.opencv.rectangle(image, (self.rang[0], self.rang[1]), (self.rang[2], self.rang[3]), (255,255,0), 2)
+                image = self.handcheck(image)
                 self.opencv.imshow('MediaPipe Hands', image)
                 if self.opencv.waitKey(5) & 0xFF == 27:
                     self.capture.release()
                     break
+
+    def handcheck(self, image):
+        image.flags.writeable = False
+        results = self.mediapipe.hands_conf.process(image)
+
+        image.flags.writeable = True
+        image = self.opencv.cvtColor(image, self.opencv.COLOR_RGB2BGR)
+        rclist = []
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                self.mediapipe.drawing.draw_landmarks(image, hand_landmarks, self.mediapipe.hands.HAND_CONNECTIONS)
+
+            count = 0
+            rst = dummy()
+            rst.x = dummy()
+            rst.y = dummy()
+
+            for landmarks in results.multi_hand_landmarks:
+                rclistb = []
+                inbox = False
+                rst.x.max = 0
+                rst.x.min = 1
+                rst.y.max = 0
+                rst.y.min = 1
+                for landmark in landmarks.landmark:
+                    rclistb.append({"x": int(landmark.x * 1366), "y": int(landmark.y * 768), "z": landmark.z})
+                    if landmark.x > rst.x.max: rst.x.max = landmark.x
+                    if landmark.x < rst.x.min: rst.x.min = landmark.x
+                    if landmark.y > rst.y.max: rst.y.max = landmark.y
+                    if landmark.y < rst.y.min: rst.y.min = landmark.y
+                
+                rst.x.max *= 1366
+                rst.x.min *= 1366
+                rst.y.max *= 768
+                rst.y.min *= 768
+
+                rst.x.max += self.rst.x.max
+                rst.x.min += self.rst.x.min
+                rst.y.max += self.rst.y.max
+                rst.y.min += self.rst.y.min
+
+                rst.x.max = int(rst.x.max)
+                rst.x.min = int(rst.x.min)
+                rst.y.max = int(rst.y.max)
+                rst.y.min = int(rst.y.min)
+                if self.storage.debug == True:
+                    print(rst.x.max)
+                    print(rst.x.min)
+                    print(rst.y.max)
+                    print(rst.y.min)
+                
+                if rst.x.max < self.rang[0] and rst.y.max < self.rang[1] and rst.x.min > self.rang[2] and rst.y.min > self.rang[3]:
+                    self.opencv.rectangle(image, (rst.x.max, rst.y.max), (rst.x.min, rst.y.min), (0,255,0), 3)
+                    rclist.append({"inbox": True, "data": rclistb})
+                    count += 1
+                else:
+                    self.opencv.rectangle(image, (rst.x.max, rst.y.max), (rst.x.min, rst.y.min), (0,0,0), 3)
+                    rclist.append({"inbox": False, "data": rclistb})
+                    pass
+
+            if count == 1:
+                if self.storage.test == True:
+                    image = self.hand_int(image, rclist, rst)
+                    pass
+
+                pass
+            elif count == 2:
+                pass
+
+            if self.storage.debug == True: print(results.multi_hand_landmarks)
+
+        self.opencv.rectangle(image, (self.rang[0], self.rang[1]), (self.rang[2], self.rang[3]), (255,255,0), 2)
+
+        return image
+
+    def hand_int(self, image, result, rst):
+        if result[0]["inbox"] == True: data = result[0]["data"]
+        else: data = result[1]["data"]
+
+        count = 0
+        mid = data[9]["y"]-20
+        if data[5]["x"] > data[9]["x"]: # 왼손
+            if data[4]["x"] > data[5]["x"]: count += 1    
+        else:# 오른손
+            if data[4]["x"] < data[5]["x"]: count += 1
+        
+        if mid > data[20]["y"]: count += 1
+        if mid > data[16]["y"]: count += 1
+        if mid > data[12]["y"]: count += 1
+        if mid > data[8]["y"]: count += 1
+
+        self.opencv.putText(image, str(count), (rst.x.min, rst.y.max), self.opencv.FONT_ITALIC, 2, self.opencv.LINE_AA)
+
+        #print(str(count)+", "+str(data[8]["y"])+", "+str(mid))
+        print(count)
+
+        return image
+        
+        pass
 
 if __name__ == "__main__":
     print("started")
